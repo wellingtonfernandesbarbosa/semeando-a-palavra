46472fa8f9c2d8dc20a152248978af28acfe23c3
Wellington Fernandes <45500846+wellingtonfernandesbarbosa@users.noreply.github.com>
2025-02-16 16:54:44 -0300

    Adiciona interface Post para tipagem de dados de postagens

---
+++ src/types/Post.ts
@@ -0,0 +1,11 @@
+export interface Post {
+  id: number;
+  title: string;
+  description: string;
+  author: string;
+  date: string;
+  image: string;
+  imageAlt: string;
+  imageTitle: string;
+  text: string;
+}

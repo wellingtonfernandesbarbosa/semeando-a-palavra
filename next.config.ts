6bce697e13cf767f8b9f2e5059e428a30d040b3f
Wellington Fernandes <45500846+wellingtonfernandesbarbosa@users.noreply.github.com>
2025-02-16 18:35:34 -0300

    Adiciona padrões remotos para carregamento de imagens no Next.js

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,38 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.freepik.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'evangelhos.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'rede.comuna.com.br',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.eusemfronteiras.com.br',
+      },
+      {
+        protocol: 'https',
+        hostname: 'f.i.uol.com.br',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.wgospel.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;

d3dfc5d57a691a82b64447e09b0e7091c1a8c4ae
Wellington Fernandes <45500846+wellingtonfernandesbarbosa@users.noreply.github.com>
2025-02-18 17:19:12 -0300

    Refatora a página inicial para incluir cabeçalho e estiliza os elementos de mensagem

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,15 @@
+import "./index.css";
+
+import Image from "next/image";
 import { GetStaticProps } from "next";
-import { getMensagens } from "../services/gistService";
+
 import { Post } from "@/types/Post";
 import Meta from "@/components/Meta";
-import "./index.css";
-import Image from "next/image";
+import { getMensagens } from "../services/gistService";
 
 type HomeProps = {
   mensagens: Post[];
 };
-
-export default function Home({ mensagens }: HomeProps) {
-  return (
-    <div className="container">
-      <Meta />
-      <h1>Semeando a Palavra</h1>
-      <div className="content">
-        {mensagens
-          .map((mensagem, index) => (
-            <div key={index} className="card">
-              <h2>{mensagem.title}</h2>
-              <p>{mensagem.author}</p>
-              <p>{mensagem.description}</p>
-              <Image src={mensagem.image} alt={mensagem.imageAlt} width={0} height={0} layout="responsive" />
-            </div>
-          ))
-          .reverse()}
-      </div>
-    </div>
-  );
-}
 
 export const getStaticProps: GetStaticProps = async () => {
   const mensagens = await getMensagens();
@@ -38,3 +19,26 @@ export const getStaticProps: GetStaticProps = async () => {
     revalidate: 60,
   };
 };
+
+export default function Home({ mensagens }: HomeProps) {
+  return (
+    <div className="container">
+      <Meta />
+      <header>
+        <h1>Semeando a Palavra</h1>
+      </header>
+      <div className="content">
+        {mensagens
+          .map((mensagem, index) => (
+            <div key={index} className="card">
+              <h2 className="title">{mensagem.title}</h2>
+              <p className="author">{mensagem.author}</p>
+              <p className="description">{mensagem.description}</p>
+              <Image src={mensagem.image} alt={mensagem.imageAlt} width={0} height={0} layout="responsive" />
+            </div>
+          ))
+          .reverse()}
+      </div>
+    </div>
+  );
+}

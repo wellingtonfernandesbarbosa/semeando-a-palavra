65cf3bce4cd46a7f730a6a96815337bd410813c3
Wellington Fernandes <45500846+wellingtonfernandesbarbosa@users.noreply.github.com>
2025-02-21 13:38:45 -0300

    Adiciona integração com @vercel/analytics para monitoramento de desempenho

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Open_Sans } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 const openSans = Open_Sans({
   subsets: ["latin"],
@@ -11,6 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className={openSans.variable}>
       <Component {...pageProps} />
+      <Analytics />
     </div>
   );
 }

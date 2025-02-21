534aeeaba4a5c8ac2e0d97391a25ccc8a7a7c88b
Wellington Fernandes <45500846+wellingtonfernandesbarbosa@users.noreply.github.com>
2025-02-21 10:08:20 -0300

    Adiciona título e descrição aos metadados da página inicial para melhorar SEO e compartilhamento em redes sociais

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,9 @@ export default function Home() {
   return (
     <>
       <Head>
+        <title>Semeando a Palavra</title>
         <meta title="Semeando a Palavra" />
+        <meta property="og:description" content="Semeando a Palavra de Deus a todos" />
         <meta name="description" content="Semeando a Palavra de Deus a todos" />
         <meta name="keywords" content="pregação, evangelho, bíblia, fé, cristianismo" />
       </Head>

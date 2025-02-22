e3032aa73053538db6dbb70c3458fba373349945
Wellington Fernandes <45500846+wellingtonfernandesbarbosa@users.noreply.github.com>
2025-02-22 10:49:20 -0300

    Aprimora estilos do cabeçalho e do BlogPost, ajustando tamanhos de fonte e centralizando o título do cabeçalho

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import styles from "./Header.module.css";
+import Link from "next/link";
 
 interface HeaderProps {
   size?: "normal" | "small";
@@ -7,7 +8,9 @@ interface HeaderProps {
 export default function Header({ size = "normal" }: HeaderProps) {
   return (
     <header className={`${styles.header} ${styles[size]}`}>
-      <h1 className={size === "small" ? styles.alignLeft : ""}>Semeando a Palavra</h1>
+      <Link href="/">
+        <h1 className={size === "small" ? styles.alignLeft : styles.center}>Semeando a Palavra</h1>
+      </Link>
     </header>
   );
 }

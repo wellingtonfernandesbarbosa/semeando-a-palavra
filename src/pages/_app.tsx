cef4269b96bf9f52a83cb761a5e34af9ba5aab86
Wellington Fernandes <45500846+wellingtonfernandesbarbosa@users.noreply.github.com>
2025-02-22 11:06:26 -0300

    Adiciona a fonte Lora ao cabeçalho e atualiza a estrutura do componente App para suportar múltiplas fontes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { Open_Sans } from "next/font/google";
+import { Open_Sans, Lora } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
 
 const openSans = Open_Sans({
@@ -8,9 +8,14 @@ const openSans = Open_Sans({
   variable: "--font-open-sans",
 });
 
+const lora = Lora({
+  subsets: ["latin"],
+  variable: "--font-lora",
+});
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className={openSans.variable}>
+    <div className={`${openSans.variable} ${lora.variable}`}>
       <Component {...pageProps} />
       <Analytics />
     </div>

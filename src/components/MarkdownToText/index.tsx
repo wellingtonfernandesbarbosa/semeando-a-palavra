b7856223b0244b7aa5b69deb4f27cef7ddd6cecd
Wellington Fernandes <45500846+wellingtonfernandesbarbosa@users.noreply.github.com>
2025-03-02 23:07:08 -0300

    adiciona renderização personalizada para links no componente MarkdownToText

--- src/components/MarkdownToText/index.tsx
+++ src/components/MarkdownToText/index.tsx
@@ -1,9 +1,18 @@
 import ReactMarkdown from "react-markdown";
+import { AnchorHTMLAttributes, DetailedHTMLProps } from "react";
 
 export default function MarkdownToText({ children }: { children: string }) {
+  const renderers = {
+    a: ({ href, children }: DetailedHTMLProps<AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement>) => (
+      <a href={href || "#"} target="_blank" rel="noopener noreferrer">
+        {children}
+      </a>
+    ),
+  };
+
   return (
     <div>
-      <ReactMarkdown>{children}</ReactMarkdown>
+      <ReactMarkdown components={renderers}>{children}</ReactMarkdown>
     </div>
   );
 }

e873f349859cf2271bd78ab412c14d58b2191091
Wellington Fernandes <45500846+wellingtonfernandesbarbosa@users.noreply.github.com>
2025-03-09 17:42:47 -0300

    Atualiza _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import "@/styles/globals.css";
+
 import type { AppProps } from "next/app";
 import { Open_Sans, Lora } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
